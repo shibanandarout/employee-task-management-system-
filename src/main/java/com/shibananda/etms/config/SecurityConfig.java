aa94d9094dab6680e0b3b934d067e03be0d0ac84
Shibananda Rout <sshibanandarout@gmail.com>
2026-07-16 19:01:31 +0530

    Implemented JWT authentication and role-based authorization

--- src/main/java/com/shibananda/etms/config/SecurityConfig.java
+++ src/main/java/com/shibananda/etms/config/SecurityConfig.java
@@ -4,7 +4,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -14,6 +16,8 @@ import com.shibananda.etms.jwt.JwtAuthenticationFilter;
 import com.shibananda.etms.serviceimpl.CustomUserDetailsService;
 
 @Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
 
 	private final JwtAuthenticationFilter jwtAuthenticationFilter;
--- src/main/java/com/shibananda/etms/controller/EmployeeController.java
+++ src/main/java/com/shibananda/etms/controller/EmployeeController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -49,6 +50,7 @@ public class EmployeeController {
 		return ResponseEntity.ok(employeeService.updateEmployee(id, employeeDTO));
 	}
 
+	@PreAuthorize("hasRole('ADMIN')")
 	@DeleteMapping("/{id}")
 	public ResponseEntity<String> deleteEmployee(@PathVariable Long id) {
 		employeeService.deleteEmployee(id);
